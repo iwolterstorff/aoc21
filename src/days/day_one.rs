b5c358f738bbab468026ec0e90c072aade72e179
Ian Wolterstorff <wolterstorff.i@northeastern.edu>
2021-12-17 21:35:44 -0500

    Refactor everything

--- src/days/day_one.rs
+++ src/days/day_one.rs
@@ -1,7 +1,28 @@
+use std::path::Path;
+
 use itertools::Itertools;
 
 use aoc21;
 
-pub fn find_number_of_increases() -> u32 {
-    let numbers = aoc21::parse_input_file("inputs/1-1.txt");
+pub fn day_1_1(path: &Path) -> i32 {
+    let input = aoc21::tokenize_file_to_integers(path).expect("Couldn't parse integers");
+    find_number_of_increases(input)
+}
+
+fn find_number_of_increases(input: Vec<i32>) -> i32 {
+    input
+        .iter()
+        .tuple_windows::<(_, _)>()
+        .fold(0, |total, (a, b)| if b > a { total + 1 } else { total + 0 })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_increases() {
+        let example = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(find_number_of_increases(example), 7);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,102 @@
 mod days;
 
+use std::convert::TryFrom;
+use std::path::Path;
+use std::str::FromStr;
+
+use anyhow::{Error, Result};
 use clap::Parser;
+
+#[derive(Debug)]
+struct Day(u16);
+
+impl Day {
+    fn new(n: u16) -> Option<Day> {
+        if n > 0 && n <= 25 {
+            Some(Day(n))
+        } else {
+            None
+        }
+    }
+}
+
+impl TryFrom<u16> for Day {
+    type Error = anyhow::Error;
+    fn try_from(value: u16) -> Result<Self> {
+        match Day::new(value) {
+            Some(d) => Ok(d),
+            None => Err(Error::msg("Invalid Day format")),
+        }
+    }
+}
+
+impl FromStr for Day {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let number: u16 = s.parse()?;
+        Day::try_from(number)
+    }
+}
+
+#[derive(Debug)]
+struct Problem(bool);
+
+impl Problem {
+    fn new(b: u16) -> Option<Problem> {
+        if b > 0 && b <= 2 {
+            Some(Problem((b - 1) != 0))
+        } else {
+            None
+        }
+    }
+}
+
+impl TryFrom<u16> for Problem {
+    type Error = anyhow::Error;
+    fn try_from(value: u16) -> Result<Self> {
+        match Problem::new(value) {
+            Some(p) => Ok(p),
+            None => Err(Error::msg("Invalid Problem format")),
+        }
+    }
+}
+
+impl FromStr for Problem {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let number: u16 = s.parse()?;
+        Problem::try_from(number)
+    }
+}
 
 #[derive(Parser, Debug)]
 #[clap(about, version)]
 struct Args {
-    #[clap(short, long)]
-    day: u32,
+    day: Day,
+    puzzle: Problem,
+}
 
-    #[clap(short, long)]
-    puzzle: u32,
+struct Solution {
+    input: &'static str,
+    function: fn(&Path) -> i32,
 }
 
 fn main() {
     let args = Args::parse();
+    let soln = get_solution_from_options(args);
+    let func = soln.function;
+    println!("{}", func(Path::new(soln.input)))
+}
+
+fn get_solution_from_options(opts: Args) -> Solution {
+    match opts {
+        Args {
+            day: Day(1),
+            puzzle: Problem(false),
+        } => Solution {
+            input: "inputs/1-1.txt",
+            function: days::day_one::day_1_1,
+        },
+        _ => panic!("You're dumb"),
+    }
 }
